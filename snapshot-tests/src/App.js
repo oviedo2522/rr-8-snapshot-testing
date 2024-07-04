55d8533191df0bfc5e03a8a9ecce9ac904afa6d8
oviedo2522 <oviedo2522@gmail.com>
2024-07-03 21:14:45 -0700

    Finish

---
+++ snapshot-tests/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+// Components
+import GitHubCard from './components/GitHubCard'
+
+function App() {
+  return (
+    <div className="App">
+      <header  className="App-header">
+        <h2>
+          Hello World!
+        </h2>
+        <GitHubCard />
+      </header >
+    </div>
+  );
+}
+
+export default App;
